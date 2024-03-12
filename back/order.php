4fc1bf456abfa1e72a5867f1295d20d940870b91
miakojason <miakojason@gmail.com>
2024-03-12 16:20:09 +0800

    更改動畫下方出現方式

---
+++ back/order.php
@@ -0,0 +1,53 @@
+<h1>訂單清單</h1>
+<div>
+    快速刪除: <input type="radio" name="" value="">
+    依日期<input type="text" name="" id="">
+    <input type="radio" name="" id="">依電影
+    <select name="" id="">
+        <option value="">電影00</option>
+    </select>
+    <button>刪除</button>
+</div>
+<div style="height: 330px;overflow:auto">
+<table style="text-align: center;">
+    <tr>
+        <th>訂單編號</th>
+        <th>電影名稱</th>
+        <th>日期</th>
+        <th>場次時間</th>
+        <th>訂購數量</th>
+        <th>訂購位置</th>
+        <th>操作</th>
+    </tr>
+    <?php
+    $rows = $Orders->all();
+    foreach ($rows as $row) {
+    ?>
+    <tr>
+        <td><?=$row['no'];?></td>
+        <td><?=$row['movie'];?></td>
+        <td><?=$row['date'];?></td>
+        <td><?=$row['session'];?></td>
+        <td><?=$row['qt'];?></td>
+        <td>
+<?php
+$seats=unserialize($row['seat']);
+foreach($seats as $seat){
+    echo floor(($seat/5)+1) . "排" . (($seat%5)+1) . "號<br>";
+}
+?>
+        </td>
+        <td><button onclick="del(<?=$row['id'];?>)">刪除</button></td>
+    </tr>
+    <?php
+    }
+    ?>
+</table>
+</div>
+<script>
+    function del(id){
+        $.post(".api/del.php",{table:'orders',id},()=>{
+            location.reload();
+        })
+    }
+</script>
--- front/main.php
+++ front/main.php
@@ -1,22 +1,62 @@
 <div class="half" style="vertical-align:top;">
-        <h1>預告片介紹</h1>
-        <div class="rb tab" style="width:95%;">
-          <div id="abgne-block-20111227">
-            <ul class="lists">
-            </ul>
-            <ul class="controls">
-            </ul>
+  <h1>預告片介紹</h1>
+  <div class="rb tab" style="width:95%;">
+    <div id="abgne-block-20111227">
+      <div class="lists" style="width: 420px;height: 300px;">
+        <?php
+        $rows = $Poster->all(['sh' => 1], " order by rank");
+        foreach ($rows as $row) {
+        ?>
+
+        <?php
+        }
+        ?>
+      </div>
+      <div class="btns" style="width: 420px;height:100px;display:flex">
+        <div onclick="pp(1)" style="display:flex;align-items:center;"><img src="./icon/l.jpg" alt=""></div>
+        <?php
+        $rows = $Poster->all(['sh' => 1]);
+        foreach ($rows as $idx => $row) {
+        ?>
+          <div class="im" id="ssaa<?= $idx; ?>">
+            <img src="./img/<?= $row['img']; ?>" style="width:95px;height:100px;">
           </div>
-        </div>
+        <?php
+        }
+        ?>
+        <div onclick="pp(2)" style="display:flex;align-items:center;"><img src="./icon/r.jpg"></div>
       </div>
-      <div class="half">
-        <h1>院線片清單</h1>
-        <div class="rb tab" style="width:95%;">
-          <table>
-            <tbody>
-              <tr> </tr>
-            </tbody>
-          </table>
-          <div class="ct"> </div>
-        </div>
-      </div>
+      <script>
+        var nowpage = 1,
+          num = <?= $Poster->count(['sh' => 1]); ?>;
+
+        function pp(x) {
+          var s, t;
+          if (x == 1 && nowpage - 1 >= 0) {
+            nowpage--;
+          }
+          if (x == 2 && nowpage < (num - 4)) {
+            nowpage++;
+          }
+          $(".im").hide()
+          for (s = 0; s <= 3; s++) {
+            t = s * 1 + nowpage * 1;
+            $("#ssaa" + t).show()
+          }
+        }
+        pp(1)
+      </script>
+    </div>
+  </div>
+</div>
+<div class="half">
+  <h1>院線片清單</h1>
+  <div class="rb tab" style="width:95%;">
+    <table>
+      <tbody>
+        <tr> </tr>
+      </tbody>
+    </table>
+    <div class="ct"> </div>
+  </div>
+</div>
